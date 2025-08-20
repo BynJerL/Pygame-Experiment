dd3290fdc78f028328f346b11331bb5b9a3d827b
BynJerL <yggcphanq@gmail.com>
2025-08-20 20:07:47 +0700

    Add Color Constants; Change background color.

--- hello_world.py
+++ hello_world.py
@@ -5,10 +5,17 @@ WINDOW_CAPTION = "Hello Pygame"
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 300
 
+# COLORS
+BASE_BG_COLOR = (16, 16, 16)
+BASE_TEXT_COLOR = (240, 240, 240)
+
 pg.init()
 
 root = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pg.display.set_caption(WINDOW_CAPTION)
+
+root.fill(BASE_BG_COLOR)
+pg.display.flip()
 
 isRunning = True
 
