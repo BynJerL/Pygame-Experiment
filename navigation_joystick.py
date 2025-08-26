951453df3a6552b32691896270bf38b3cd063d8c
BynJerL <yggcphanq@gmail.com>
2025-08-26 16:33:52 +0700

    Implement joystick control on the template.

---
+++ navigation_joystick.py
@@ -0,0 +1,261 @@
+import pygame as pg
+
+class Player:
+    def __init__(self, width: int = 20, height:int = 20, speed = 1.5):
+        self.width = width
+        self.height = height
+        self.posX = 75 - width // 2
+        self.posY = 75 - height // 2
+        self.lastX = self.posX
+        self.lastY = self.posY
+        self.speed = speed
+
+        self.isMoving = False
+        self.direction = None
+    
+    def getSprite(self):
+        leftLimit = WINDOW_WIDTH // 2 - 75
+        rightLimit = WINDOW_WIDTH // 2 + 75 - self.width
+        topLimit = 115
+        bottomLimit = topLimit + 150 - self.height
+        absX = min(rightLimit, max(leftLimit, self.posX + leftLimit))
+        absY = min(bottomLimit, max(topLimit, self.posY + topLimit))
+        return pg.Rect(int(absX), int(absY), self.width, self.height)
+
+    def draw(self):
+        leftLimit = WINDOW_WIDTH // 2 - 75
+        rightLimit = WINDOW_WIDTH // 2 + 75
+        topLimit = 115
+        bottomLimit = 265
+        absX = min(rightLimit, max(leftLimit, self.posX + leftLimit))
+        absY = min(bottomLimit, max(topLimit, self.posY + topLimit))
+        sprite = pg.Rect(int(absX), int(absY), self.width, self.height)
+        pg.draw.rect(root, (240, 0, 0), sprite)
+    
+    def moveLeft(self):
+        self.posX = max(0, self.posX - self.speed)
+        self.isMoving = True
+    
+    def moveRight(self):
+        self.posX = min(150 - self.width, self.posX + self.speed)
+        self.isMoving = True
+    
+    def moveTop(self):
+        self.posY = max(0, self.posY - self.speed)
+        self.isMoving = True
+    
+    def moveBottom(self):
+        self.posY = min(150 - self.height, self.posY + self.speed)
+        self.isMoving = True
+    
+    def getLastCoor(self):
+        self.lastX = self.posX
+        self.lastY = self.posY
+
+    def getDirection(self):
+        dx = self.posX - self.lastX
+        dy = self.posY - self.lastY
+
+        if dx == 0 and dy == 0:
+            self.direction = None
+        elif dx > 0 and dy == 0:
+            self.direction = "E"
+        elif dx > 0 and dy > 0:
+            self.direction = "SE"
+        elif dx == 0 and dy > 0:
+            self.direction = "S"
+        elif dx < 0 and dy > 0:
+            self.direction = "SW"
+        elif dx < 0 and dy == 0:
+            self.direction = "W"
+        elif dx < 0 and dy < 0:
+            self.direction = "NW"
+        elif dx == 0 and dy < 0:
+            self.direction = "N"
+        elif dx > 0 and dy < 0:
+            self.direction = "NE"
+
+    def isCollide(self, collider: "Follower"):
+        return self.getSprite().colliderect(collider.getSprite())
+
+    def idling(self):
+        self.isMoving = False
+        self.direction = None
+
+class Follower:
+    def __init__(self, width: int = 15, height: int = 15, speed: float = 1.0):
+        self.width = width
+        self.height = height
+        self.speed = speed
+        self.posX = 10
+        self.posY = 10
+        self.stepOp = "trig"
+    
+    def _arenaLimits(self):
+        return (WINDOW_WIDTH // 2 - 75, 115) # (x, y)
+    
+    def getSprite(self):
+        leftLimit, topLimit = self._arenaLimits()
+        rightLimit, bottomLimit = (leftLimit + 150 - self.width, topLimit + 150 - self.height)
+        absX = min(rightLimit, max(leftLimit, leftLimit + self.posX))
+        absY = min(bottomLimit, max(topLimit, topLimit + self.posY))
+        return pg.Rect(int(absX), int(absY), self.width, self.height)
+    
+    def update(self, player: "Player"):
+        if self.getSprite().colliderect(player.getSprite()):
+            return
+        
+        tX = player.posX + player.width // 2
+        tY = player.posY + player.height // 2
+        rX = self.posX + self.width // 2
+        rY = self.posY + self.height // 2
+
+        dx = tX - rX
+        dy = tY - rY
+        drSq = dx ** 2 + dy ** 2
+        if drSq == 0:
+            return
+        
+        dr = drSq ** 0.5
+        match (self.stepOp):
+            case "trig":
+                stepX = (dx / dr) * self.speed
+                stepY = (dy / dr) * self.speed
+            case "round":
+                stepX = round(dx / dr) * self.speed
+                stepY = round(dy / dr) * self.speed
+            case _:
+                stepX = (dx / dr) * self.speed
+                stepY = (dy / dr) * self.speed
+
+        self.posX += stepX
+        self.posY += stepY
+
+        self.posX = min(150 - self.width, max(0, self.posX))
+        self.posY = min(150 - self.height, max(0, self.posY))
+    
+    def draw(self):
+        pg.draw.rect(root, (240, 240, 0), self.getSprite())
+
+# SETUPS
+WINDOW_CAPTION = "Hello Pygame"
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 300
+FPS = 60
+ICON_ADDRESS = "assets/icon/icon.png"
+
+# COLORS
+BASE_BG_COLOR = (16, 16, 16)
+BASE_TEXT_COLOR = (240, 240, 240)
+
+# INITIALIZATION
+pg.init()
+
+# JOYSTICK SETUP
+if pg.joystick.get_count() > 0:
+    joystick = pg.joystick.Joystick(0)
+    joystick.init()
+else:
+    joystick = None
+
+root = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pg.display.set_caption(WINDOW_CAPTION)
+pg_icon = pg.image.load(ICON_ADDRESS)
+pg.display.set_icon(pg_icon)
+
+root.fill(BASE_BG_COLOR)
+
+clock = pg.time.Clock()
+
+# OBJECT DRAW
+player = Player()
+follower = Follower()
+arena = pg.Rect(WINDOW_WIDTH // 2 - 75, 115, 150, 150)
+pg.draw.rect(root, (240, 0, 0), pg.Rect(30, 30, 30, 30))
+pg.draw.rect(root, BASE_TEXT_COLOR, arena, 3)
+player.draw()
+# fonts = pg.font.get_fonts()
+# for f in fonts:
+#     print(f)
+
+heading1 = pg.font.SysFont("consolas", 30, bold=True, italic=True)
+normalText = pg.font.SysFont("arial", 20, bold=False, italic=False)
+subText = pg.font.SysFont("consolas", 12, bold=True)
+text1 = heading1.render("Hello, Pygame!", True, BASE_TEXT_COLOR)
+text2 = normalText.render("Hello, World! This is only a template.", True, BASE_TEXT_COLOR)
+root.blit(text1, (75, 30))
+root.blit(text2, (30, 65))
+
+# FLIP
+pg.display.flip()
+
+# LOOP
+isRunning = True
+while isRunning:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            isRunning = False
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_r:
+                follower.stepOp = "round"
+            if event.key == pg.K_t:
+                follower.stepOp = "trig"
+    
+    player.getLastCoor()
+
+    moved = False
+    if joystick:
+        x_axis = joystick.get_axis(0)
+        y_axis = joystick.get_axis(1)
+        threshold = 0.2
+
+        if y_axis < -threshold:
+            player.moveTop()
+            moved = True
+        if y_axis > threshold:
+            player.moveBottom()
+            moved = True
+        if x_axis < -threshold:
+            player.moveLeft()
+            moved = True
+        if x_axis > threshold:
+            player.moveRight()
+            moved = True
+
+    if not moved:
+        keys = pg.key.get_pressed()
+        if keys[pg.K_UP]:
+            player.moveTop()
+        if keys[pg.K_DOWN]:
+            player.moveBottom()
+        if keys[pg.K_LEFT]:
+            player.moveLeft()
+        if keys[pg.K_RIGHT]:
+            player.moveRight()
+    
+    follower.update(player)
+    player.getDirection()
+
+    if player.direction == None:
+        player.isMoving = False
+
+    isCollide = player.isCollide(follower)
+
+    root.blit(subText.render(f"chase: {follower.stepOp}  ", True, BASE_TEXT_COLOR, BASE_BG_COLOR),  (5, WINDOW_HEIGHT - 82))
+    root.blit(subText.render(f"touching: {isCollide}  ", True, BASE_TEXT_COLOR, BASE_BG_COLOR),  (5, WINDOW_HEIGHT - 68))
+    root.blit(subText.render(f"is_moving: {player.isMoving}  ", True, BASE_TEXT_COLOR, BASE_BG_COLOR),  (5, WINDOW_HEIGHT - 54))
+    root.blit(subText.render(f"dir: {player.direction}   ", True, BASE_TEXT_COLOR, BASE_BG_COLOR),  (5, WINDOW_HEIGHT - 40))
+    root.blit(subText.render(f"coor: (x={player.posX},y={player.posY})      ", True, BASE_TEXT_COLOR, BASE_BG_COLOR), (5, WINDOW_HEIGHT - 28))
+    
+    root.fill(BASE_BG_COLOR, arena)
+    pg.draw.rect(root, BASE_TEXT_COLOR, arena, 3)
+    follower.draw()
+    player.draw()
+
+    clock.tick(FPS)
+    currentfps = round(clock.get_fps(), 2)
+
+    root.blit(subText.render(f"fps: {currentfps} ", True, BASE_TEXT_COLOR, BASE_BG_COLOR), (5, WINDOW_HEIGHT - 15))
+
+    pg.display.flip()
+pg.quit()
